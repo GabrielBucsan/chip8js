e25b2cdfe5521a70e9eb70182da12be378b2514a
Gabriel Bucsan <gabriel@bucsan.com.br>
2021-01-02 21:49:40 -0300

    Deploy

--- chip8-front/src/emulator/memory.js
+++
@@ -1,7 +0,0 @@
-const createMemory = (sizeInBytes) => {
-  const buffer = new ArrayBuffer(sizeInBytes);
-  const dataView = new DataView(buffer);
-  return dataView;
-};
-
-export default { createMemory };
--- chip8-front/src/emulator/renderer.js
+++
@@ -1,3 +0,0 @@
-const updateRegisters = function(){
-    
-}
