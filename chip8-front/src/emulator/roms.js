199dcf79a40f404a358a05b2e26e9e5f8119bbe5
Gabriel Bucsan <gabriel@bucsan.com.br>
2021-01-02 21:48:52 -0300

    Added more roms

--- chip8-front/src/emulator/roms.js
+++ chip8-front/src/emulator/roms.js
@@ -11,6 +11,11 @@ import maze from './roms/maze.js';
 import life from './roms/life.js';
 import intro from './roms/intro.js';
 import rndTest from './roms/rndTest.js';
+import fishie from './roms/fishie.js';
+import particles from './roms/particles.js';
+import sierpinski from './roms/sierpinski.js';
+import bowling from './roms/bowling.js';
+import rushhour from './roms/rushhour.js';
 
 const roms = [
   {
@@ -18,41 +23,57 @@ const roms = [
     id: 'intro',
     file: intro
   },{
-    name: 'Chip8 Logo',
-    id: 'chip8Logo',
-    file: chip8Logo
-  },{
-    name: 'Space Invaders',
-    id: 'invaders',
-    file: invaders
-  },{
-    name: 'Tetris',
-    id: 'tetris',
-    file: tetris
-  },{
-    name: 'Pong',
-    id: 'pong',
-    file: pong
-  },{
-    name: 'Trip8',
-    id: 'trip8',
-    file: trip8
-  },{
-    name: 'Puzzle 15',
-    id: 'puzzle15',
-    file: puzzle15
+    name: 'Bowling',
+    id: 'bowling',
+    file: bowling
   },{
     name: 'Brix',
     id: 'brix',
     file: brix
   },{
-    name: 'Maze',
-    id: 'maze',
-    file: maze
+    name: 'Chip8 Logo',
+    id: 'chip8Logo',
+    file: chip8Logo
+  },{
+    name: 'Fishie',
+    id: 'fishie',
+    file: fishie
   },{
     name: 'Life',
     id: 'life',
     file: life
+  },{
+    name: 'Maze',
+    id: 'maze',
+    file: maze
+  },{
+    name: 'Particles',
+    id: 'particles',
+    file: particles
+  },{
+    name: 'Pong',
+    id: 'pong',
+    file: pong
+  },{
+    name: 'Puzzle 15',
+    id: 'puzzle15',
+    file: puzzle15
+  },{
+    name: 'Random Number Test',
+    id: 'rndTest',
+    file: rndTest
+  },{
+    name: 'Rush Hour',
+    id: 'rushhour',
+    file: rushhour
+  },{
+    name: 'Sierpinski',
+    id: 'sierpinski',
+    file: sierpinski
+  },{
+    name: 'Space Invaders',
+    id: 'invaders',
+    file: invaders
   },{
     name: 'Test 1',
     id: 'test1',
@@ -62,9 +83,13 @@ const roms = [
     id: 'test2',
     file: test2
   },{
-    name: 'Random Number Test',
-    id: 'rndTest',
-    file: rndTest
+    name: 'Tetris',
+    id: 'tetris',
+    file: tetris
+  },{
+    name: 'Trip8',
+    id: 'trip8',
+    file: trip8
   }
 ]
 
